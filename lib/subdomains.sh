618bec6f16708c2c682f0fe1f3d8a69bf6c37cd5
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2025-04-16 21:59:48 +0330

    Update subdomains.sh

--- lib/subdomains.sh
+++ lib/subdomains.sh
@@ -7,6 +7,7 @@ RESET='\033[0m'
 subdomains()
 {
     subfinder -d $DOMAIN -all -recursive -silent -o $TMP/sub_one.txt
+    assetfinder $DOMAIN >> $TMP/sub_one.txt
     curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' >> $TMP/sub_one.txt
     httpx -l $TMP/sub_one.txt -resolve -silent -o $TMP/sub_two.txt
     cat $TMP/sub_two.txt | sort -u | anew $TMP/sub_three.txt > $TMP/subdomains.txt
