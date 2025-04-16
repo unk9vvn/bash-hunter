64dc1ea139fc79ef0a3a201dc0725018ba3e3309
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2025-04-16 10:35:34 +0330

    Update ports.sh

--- lib/ports.sh
+++ lib/ports.sh
@@ -3,6 +3,16 @@
 ports()
 {
     naabu -list $TMP/subdomains.txt -silent -o $TMP/port_one.txt
+    cat $TMP/subdomains.txt | while read domain; do
+        if ping -c 1 $domain &> /dev/null; then
+            rustscan_output=$(rustscan -a $domain --ulimit 5000 -- -Pn -T4 -n 2>&1)
+            if echo "$rustscan_output" | grep -q "PORT"; then
+                echo "$rustscan_output" | grep -E "^[0-9]+/tcp" | awk '{print $1}' | cut -d "/" -f1 | while read port_number; do
+                    echo "$domain:$port_number" >> $TMP/port_one.txt
+                done
+            fi
+        fi
+    done
     httpx -l $TMP/port_one.txt -resolve -silent -o $TMP/port_two.txt
     cat $TMP/port_two.txt | sort -u | anew $TMP/port_three.txt > $TMP/ports.txt
     rm -f $TMP/port_one.txt $TMP/port_two.txt $TMP/port_three.txt
